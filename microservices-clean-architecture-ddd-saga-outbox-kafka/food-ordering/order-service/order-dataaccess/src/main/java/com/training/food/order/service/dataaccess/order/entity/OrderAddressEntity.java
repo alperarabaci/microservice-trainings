c241816abcafd33e5fc16a185f0c6e511068d7a1
alperarabaci <melihalper.arabaci@gmail.com>
2022-10-18 15:48:56 +0300

    L33-Data access module: Adding Order JPA Entity

---
+++ microservices-clean-architecture-ddd-saga-outbox-kafka/food-ordering/order-service/order-dataaccess/src/main/java/com/training/food/order/service/dataaccess/order/entity/OrderAddressEntity.java
@@ -0,0 +1,43 @@
+package com.training.food.order.service.dataaccess.order.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name="order_address")
+public class OrderAddressEntity {
+
+    @Id
+    private UUID id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name= "ORDER_ID")
+    private OrderEntity order;
+
+    private String street;
+
+    private String postalCode;
+
+    private String city;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderAddressEntity that = (OrderAddressEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
