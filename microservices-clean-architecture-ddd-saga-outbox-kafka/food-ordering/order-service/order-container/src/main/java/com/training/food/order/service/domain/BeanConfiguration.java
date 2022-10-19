5f3460762dd1e51fa89668f93e919951869bd8fe
alperarabaci <melihalper.arabaci@gmail.com>
2022-10-19 23:36:41 +0300

    L39, 40, 41
    39-Container module: Adding spring boot starter class and config
    40-Container module: Explaining kafka consumer properties
    41-Container module: Adding Order database schema file

---
+++ microservices-clean-architecture-ddd-saga-outbox-kafka/food-ordering/order-service/order-container/src/main/java/com/training/food/order/service/domain/BeanConfiguration.java
@@ -0,0 +1,20 @@
+package com.training.food.order.service.domain;
+
+import com.training.food.ordering.order.service.domain.OrderDomainService;
+import com.training.food.ordering.order.service.domain.OrderDomainServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    /**
+     * We dont use spring features in domain. So we have to define its service manually.
+     * Application domain doesn't have any dependency to spring framework
+     * @return
+     */
+    @Bean
+    public OrderDomainService orderDomainService() {
+        return new OrderDomainServiceImpl();
+    }
+}
