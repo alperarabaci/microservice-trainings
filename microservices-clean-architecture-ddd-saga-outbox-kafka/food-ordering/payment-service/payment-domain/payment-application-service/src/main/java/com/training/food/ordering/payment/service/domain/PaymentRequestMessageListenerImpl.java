40786bdc63417cf4052af51c73ab60c96096c615
alperarabaci <melihalper.arabaci@gmail.com>
2022-10-21 12:55:58 +0300

    L48-Application Service domain module: Implementing input ports - Part 2

--- microservices-clean-architecture-ddd-saga-outbox-kafka/food-ordering/payment-service/payment-domain/payment-application-service/src/main/java/com/training/food/ordering/payment/service/domain/PaymentRequestMessageListenerImpl.java
+++ microservices-clean-architecture-ddd-saga-outbox-kafka/food-ordering/payment-service/payment-domain/payment-application-service/src/main/java/com/training/food/ordering/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -1,18 +1,54 @@
 package com.training.food.ordering.payment.service.domain;
 
 import com.training.food.ordering.payment.service.domain.dto.PaymentRequest;
+import com.training.food.ordering.payment.service.domain.event.PaymentCanceledEvent;
+import com.training.food.ordering.payment.service.domain.event.PaymentCompletedEvent;
+import com.training.food.ordering.payment.service.domain.event.PaymentEvent;
+import com.training.food.ordering.payment.service.domain.event.PaymentFailedEvent;
 import com.training.food.ordering.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import com.training.food.ordering.payment.service.domain.ports.output.message.publisher.PaymentCanceledMessagePublisher;
+import com.training.food.ordering.payment.service.domain.ports.output.message.publisher.PaymentCompletedMessagePublisher;
+import com.training.food.ordering.payment.service.domain.ports.output.message.publisher.PaymentFailedMessagePublisher;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
+@Slf4j
 public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    private final PaymentRequestHelper paymentRequestHelper;
+    private final PaymentCompletedMessagePublisher completedMessagePublisher;
+    private final PaymentCanceledMessagePublisher canceledMessagePublisher;
+    private final PaymentFailedMessagePublisher failedMessagePublisher;
 
 
     @Override
     public void completePayment(PaymentRequest paymentRequest) {
-
+        PaymentEvent event = paymentRequestHelper.persistPayment(paymentRequest);
+        fireEvent(event);
     }
 
     @Override
     public void cancelPayment(PaymentRequest paymentRequest) {
-
+        PaymentEvent event = paymentRequestHelper.persistCancelPayment(paymentRequest);
+        fireEvent(event);
     }
+
+    private void fireEvent(PaymentEvent event) {
+        log.info("Publishing payment event with payment id: {} and order id: {}",
+                event.getPayment().getId(),
+                event.getPayment().getOrderId().getValue());
+        //TODO need refactoring
+        if (event instanceof PaymentCompletedEvent) {
+            completedMessagePublisher.publish((PaymentCompletedEvent) event);
+        } else if (event instanceof PaymentFailedEvent) {
+            failedMessagePublisher.publish((PaymentCompletedEvent) event);
+        } else if (event instanceof PaymentCanceledEvent) {
+            canceledMessagePublisher.publish((PaymentCompletedEvent) event);
+        }
+    }
+
+
 }
